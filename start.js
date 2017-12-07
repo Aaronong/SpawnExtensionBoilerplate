9a16474878ce9a8e22ead1aa49bf70b79d670e75
Aaron Ong <aaronongcs@gmail.com>
2017-12-08 00:04:33 +0800

    Entry point of application updated to extension.js.

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 "use strict";
 let { dataTypes, coreFunctions } = require("spawn-extension");
-let extension = require("./index");
+let extension = require("./extension");
 
 extension(coreFunctions, dataTypes);
