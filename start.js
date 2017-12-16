921c1dd892323e22b5abd1279ffde1bf2e1bbd3a
Aaron Ong <aaronongcs@gmail.com>
2017-12-16 19:27:30 +0800

    update boilerplate to be compatible with spawn-extension@1.0.3

--- start.js
+++ start.js
@@ -1,9 +1,10 @@
 "use strict";
-let { dataTypes, coreFunctions } = require("spawn-extension");
+let { dataTypes, coreContext, extensionRecords } = require("spawn-extension");
 let extension = require("./extension");
 
 // These constants are irrelevant. Used in server for deconflicting.
 const name = "extensionName";
 const version = "extensionVersion";
+let coreFunctions = coreContext("extName", "extVersion");
 
-extension(coreFunctions(name, version), dataTypes);
+extension(coreFunctions, dataTypes);
